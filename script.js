17417a699b256419a6aa4e2d3d1c4a316c036c4e
Bobikk <65063261+Bobikk@users.noreply.github.com>
2020-07-18 14:10:20 +0300

    MASSIVE UPDATE

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+let download_b = document.querySelector('.download-page')
+download_b.addEventListener('click', function(){
+    document.location.href = "Downloads/downloads.html";
+})
